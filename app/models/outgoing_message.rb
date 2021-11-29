4948073b30f37898f52670e3dcf588812957de6d
Gareth Rees <gareth@garethrees.co.uk>
2021-11-29 17:36:46 +0000

    fixup! Strip text for indexing after salutation stripping

--- app/models/outgoing_message.rb
+++ app/models/outgoing_message.rb
@@ -348,7 +348,7 @@ class OutgoingMessage < ApplicationRecord
 
     if strip_salutation && public_body
       salutation = self.class.default_salutation(public_body)
-      text.sub!(/#{salutation}\s*/, '')
+      text.sub!(/#{Regexp.escape(salutation)}\s*/, '')
     end
 
     # Remove email addresses from display/index etc.
